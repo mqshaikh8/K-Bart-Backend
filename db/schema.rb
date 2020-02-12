6fbc78b2ccd46db1f7ebc38e48df032a0f63eeed
scypher6 <scypher6@gmail.com>
2020-02-12 09:36:42 -0500

    Added image column to items and and reseeded without faker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_212012) do
+ActiveRecord::Schema.define(version: 2020_02_12_023105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_07_212012) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
